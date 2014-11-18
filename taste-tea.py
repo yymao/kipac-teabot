fa4c1ee10cc4a680c9c73cf91237ccfe57620f5b
Yao-Yuan Mao <yymao@stanford.edu>
2014-11-17 16:06:04 -0800

    bug fix again

--- taste-tea.py
+++ taste-tea.py
@@ -12,16 +12,14 @@ key = form.getvalue('key')
 
 if name and key:
     import md5
-    import sys
-    sys.path.append('../kipac-teabot')
     from database import keypass, model_dir
     if md5.md5(arxiv_id + name + keypass).hexdigest() == key:
         import anydbm
         d = anydbm.open('%s/%s'%(model_dir, name), 'w')
         if arxiv_id not in d or float(d[arxiv_id]) < score:
-            d[arxiv_id] = score
+            d[arxiv_id] = str(score)
         d.close()
 
-url = 'http://arxiv.org/pdf/%s.pdf'%(arxiv_id) if arxiv_id else 'http://arxiv.org'
+url = ('http://arxiv.org/pdf/%s.pdf'%(arxiv_id)) if arxiv_id else 'http://arxiv.org'
 print '<html><head><script>location.href="%s"</script><meta http-equiv="Refresh" content="0; URL=%s"></head></html>'%(url, url)
 
