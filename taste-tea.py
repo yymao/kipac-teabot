0403610983224bdaa1297a5648da30a2066343bc
Yao-Yuan Mao <yymao@stanford.edu>
2015-01-21 10:46:05 -0800

    bug fix in taste-tea.py

--- taste-tea.py
+++ taste-tea.py
@@ -9,13 +9,12 @@ key = form.getvalue('key')
 
 if name and key:
     import md5
-    from database import keypass, model_dir
+    from secrets import keypass
     if md5.md5(arxiv_id + name + keypass).hexdigest() == key:
-        import anydbm
-        d = anydbm.open('%s/%s'%(model_dir, name), 'w')
-        if arxiv_id not in d or float(d[arxiv_id]) < score:
-            d[arxiv_id] = str(score)
-        d.close()
+        from Member import Member
+        with Member(name).get_weights_db('w') as d:
+            if arxiv_id not in d or float(d[arxiv_id]) < score:
+                d[arxiv_id] = str(score)
 
 url = ('http://arxiv.org/pdf/%s.pdf'%(arxiv_id)) if arxiv_id else 'http://arxiv.org'
 
