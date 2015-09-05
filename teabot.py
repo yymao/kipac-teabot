a0fa32204a1af410e3761d97fa465909cfad3a2c
Yao-Yuan Mao <yymao@stanford.edu>
2015-09-05 13:18:15 -0700

    print out full list

--- teabot.py
+++ teabot.py
@@ -23,6 +23,7 @@ from topic_model import topic_model, collection_weight, similarity_threshold
 from secrets import keypass, member_list_path, collection_weight_path
 from Member import Member
 
+
 #get new arxiv entries
 arxiv = fetch_arxiv(max_results=200, \
         search_query='cat:astro-ph*+AND+submittedDate:[{0[0]}+TO+{0[1]}]'.format(\
@@ -31,6 +32,7 @@ entries = arxiv.getentries()
 if len(entries) == 0:
     sys.exit(0)
 del arxiv
+
 
 #load kipac members
 people = []
@@ -43,6 +45,7 @@ with open(member_list_path, 'r') as f:
             people.append(row)
 if len(people) == 0:
     sys.exit(0)
+
 
 #apply collection weight to arxiv entries
 with open(collection_weight_path, 'rb') as f:
@@ -57,6 +60,7 @@ for entry in entries:
         scores.append(model.get_similarity(person['model']))
 scores = np.array(scores).reshape(len(entries), len(people))
 
+
 #clean up
 del model
 del cw
@@ -65,15 +69,18 @@ tester_idx = []
 for i, person in enumerate(people):
     if int(person['active'] or 0):
         active_idx.append(i)
-    if int(person['tester'] or 0):
+    person['tester'] = int(person['tester'] or 0)
+    if person['tester']:
         tester_idx.append(i)
     del person['model']
     del person['active']
-    del person['tester']
+
 
 #helper functions
-def get_largest_indices(scores, limit, threshold=similarity_threshold):
+def get_largest_indices(scores, limit, threshold=similarity_threshold, store_argsort=None):
     s = scores.argsort()
+    if store_argsort is not None:
+        store_argsort[:] = s[::-1]
     for c, i in enumerate(reversed(s)):
         if scores[i] < threshold or c >= limit:
             break
@@ -85,11 +92,21 @@ def format_authors(authors, max_authors=6):
         a += u' et al.'
     return cgi.escape(a)
 
+def format_entry(entry, arxivname, print_abstract=True, score=None):
+    arxiv_id = entry['key']
+    key = md5.md5(arxiv_id + arxivname + keypass).hexdigest()
+    url = 'https://web.stanford.edu/~yymao/cgi-bin/kipac-teabot/taste-tea.py?id={0}&name={1}&key={2}'.format(arxiv_id, arxivname, key)
+    abstract = u'{0} [<a href="{1}">Read more</a>] <br><br><br>'.format(cgi.escape(entry['summary']), url) if print_abstract else u''
+    score = u' (score = {:.3g})'.format(score) if score is not None else u''
+    return u'<li>[<a href="{0}&abs=on">{1}</a>] <b><a href="{0}">{2}</a></b>{5} <br>by {3} <br><br>{4}</li>'.format(\
+                url, arxiv_id, cgi.escape(entry['title']), format_authors(entry['authors']), abstract, score)
+
 #start an email server
 email = email_server()
 from_me = 'KIPAC TeaBot <teabot@kipac.stanford.edu>'
 footer =  u'<br><p>This message is automatically generated and sent by KIPAC TeaBot. <br>'
 footer += u'<a href="https://github.com/yymao/kipac-teabot/issues?state=open">Create an issue</a> if you have any suggestions/questions.</p>'
+
 
 #find papers that members are interested
 n_papers = 8
@@ -113,29 +130,38 @@ if any_paper:
             '[TeaBot] {0} new papers on arXiv'.format(time.strftime('%m/%d',time.localtime())), \
             msg + footer)
 
+
 #find interesting papers for individual members
 n_papers = 3
+n_full_output = 50
 for j in tester_idx:
     person = people[j]
-    msg = u'Hi {0}, <br><br>'.format(person['nickname'])
-    msg += u'TeaBot thinks you\'ll find the following paper(s) on arXiv today interesting: <br>'
+    do_full_output = (person['tester'] == 2)
+    greetings = u'Hi {0}, <br><br>'.format(person['nickname'])
+    msg = u'TeaBot thinks you\'ll find the following paper(s) on arXiv today interesting: <br>'
     msg += u'<ul>'
-    any_paper = False
-    for i in get_largest_indices(scores[:, j], n_papers):
-        entry = entries[i]
+    any_paper = 0
+    ss = np.empty(len(scores), int) if do_full_output else None
+    for i in get_largest_indices(scores[:, j], n_papers, store_argsort=ss):
         if not any_paper:
-            best_title = entry['title']
-            any_paper = True
-        arxiv_id = entry['key']
-        key = md5.md5(arxiv_id + person['arxivname'] + keypass).hexdigest()
-        url = 'https://web.stanford.edu/~yymao/cgi-bin/kipac-teabot/taste-tea.py?id={0}&name={1}&key={2}'.format(arxiv_id, person['arxivname'], key)
-        msg += u'<li>[<a href="{0}&abs=on">{1}</a>] <b><a href="{0}">{2}</a></b> <br>by {3} <br><br>{4} [<a href="{0}">Read more</a>]<br><br><br></li>'.format(\
-                url, arxiv_id, cgi.escape(entry['title']), format_authors(entry['authors']), cgi.escape(entry['summary']))
-    if not any_paper:
-        continue
+            best_title = entries[i]['title']
+        msg += format_entry(entries[i], person['arxivname'])
+        any_paper += 1
     msg += u'</ul>'
-    email.send(from_me, '{0[name]} <{0[email]}>'.format(person), \
-            '[TeaBot] ' + best_title, msg + footer)
+    if do_full_output:
+        if not any_paper:
+            any_paper += 1
+            best_title = '{0} new papers on arXiv'.format(time.strftime('%m/%d',time.localtime()))
+            msg = u''
+        msg += u'The following papers are sorted by relevance:'
+        msg += u'<ul>'
+        for i in ss[any_paper:n_full_output]:
+            msg += format_entry(entries[i], person['arxivname'], False)
+        msg += u'</ul>'
+    if any_paper:
+        email.send(from_me, '{0[name]} <{0[email]}>'.format(person), \
+                '[TeaBot] ' + best_title, u''.join((greetings, msg, footer)))
+
 
 #close the email server
 email.close()
