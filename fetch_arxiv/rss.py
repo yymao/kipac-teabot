a7863bf13c427c33b4eb4d4729dfb1a522e15762
Yao-Yuan Mao <yymao.astro@gmail.com>
2024-05-16 21:13:03 -0600

    fix arxiv rss

--- fetch_arxiv/rss.py
+++ fetch_arxiv/rss.py
@@ -3,20 +3,11 @@ import re
 import datetime
 import xml.etree.ElementTree as ET
 
-try:
-    from HTMLParser import HTMLParser
-except ImportError:
-    from html import unescape
-else:
-    _html_parser = HTMLParser()
-    def unescape(s):
-        return _html_parser.unescape(s)
-
 from .utils import load_url
 
 __all__ = ["fetch_arxiv_rss"]
 
-_url_base = "http://export.arxiv.org/rss/astro-ph"
+_url_base = "https://rss.arxiv.org/rss/astro-ph"
 _ns = {"arxiv": "http://arxiv.org/schemas/atom", "dc":"http://purl.org/dc/elements/1.1/"}
 
 
@@ -28,22 +19,21 @@ class arxiv_entry:
     def __getattr__(self, name):
         if name not in self._attr_cache:
             if name == "authors":
-                output = [unescape(a.text).strip() for a in self.entry.find("dc:creator", _ns).findall("a")]
+                output = [a.strip() for a in re.sub(r"\(.*?\)", "", self.entry.findtext("dc:creator", "", _ns)).split(",")]
             elif name == "first_author":
                 output = self.authors[0]
             elif name == "authors_text":
                 output = ", ".join(self.authors)
             elif name in ("key", "id"):
-                output = self.entry.findtext("link").partition("arxiv.org/abs/")[-1].strip()
+                output = self.entry.findtext("link").partition("/abs/")[-1]
             elif name == "title":
-                output = self.entry.findtext("title").partition(". (arXiv:")[0].strip()
+                output = self.entry.findtext("title").strip()
             elif name == "summary":
-                summary_raw = self.entry.findtext("description", "", _ns)
-                output = re.sub(r"\s+", " ", re.sub(r"<[^<>]*>", "", summary_raw)).strip()
+                output = self.entry.findtext("description", "", _ns).partition("Abstract:")[-1].strip()
             elif name == "is_new":
                 output = self.entry.findtext("arxiv:announce_type", "", _ns).strip() in ["new", "cross"]
             else:
-                output = self.entry.findtext("rss:{}".format(name), None, _ns)
+                output = self.entry.findtext(name, None, _ns)
 
             if output is not None:
                 self._attr_cache[name] = output
@@ -51,6 +41,9 @@ class arxiv_entry:
         return self._attr_cache[name]
 
     __getitem__ = __getattr__
+
+    def __repr__(self):
+        return "{}".format(self.entry)
 
 
 class fetch_arxiv_rss:
@@ -68,7 +61,7 @@ class fetch_arxiv_rss:
     def date(self):
         date_str = self.root.findtext("pubDate")
         date_str = " ".join(date_str.split()[1:4])
-        return datetime.datetime.strptime("02 Feb 2024", "%d %b %Y").strftime("%Y%m%d")
+        return datetime.datetime.strptime(date_str, "%d %b %Y").strftime("%Y%m%d")
 
     @property
     def entries(self):
